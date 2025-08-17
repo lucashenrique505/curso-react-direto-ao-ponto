aba3864f16ef77588ee46791fceb9d94766c2529
lucashenrique505 <lucashenriqued092@gmail.com>
2025-08-17 19:17:15 -0300

    projeto password-generator

---
+++ password-generator/src/App.jsx
@@ -0,0 +1,16 @@
+import PasswordGenerator from "./components/PasswordGenerator";
+
+function App() {
+  return (
+    <>
+      <main>
+        <div className="container">
+          <h1 className="app-title">Gerador de Senhas</h1>
+          <PasswordGenerator />
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ password-generator/src/components/CopyPasswordButton/index.jsx
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+const CopyPasswordButton = ({ password }) => {
+  const copyPasswordToClipboard = () => {
+    navigator.clipboard.writeText(password);
+  };
+
+  return (
+    <div className="actions">
+      <button
+        className="copy-password-to-clipboard"
+        onClick={copyPasswordToClipboard}
+      >
+        Copiar para a área de transferência
+      </button>
+    </div>
+  );
+};
+
+export default CopyPasswordButton;
